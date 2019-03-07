6eb038d46e00855a4e78ed467d092eaa0d2d124a
samuelhegner <C16439262@mydit.ie>
2019-03-07 23:16:52 +0000

    Stuff

--- GE Assignment Semester 2/Assets/Scripts/Boid.cs
+++ GE Assignment Semester 2/Assets/Scripts/Boid.cs
@@ -19,6 +19,15 @@ public class Boid : MonoBehaviour
     public float maxSpeed = 5.0f;
     public float maxForce = 10.0f;
 
+    public bool bank;
+
+    public enum BankMode {
+        correct,
+        stay
+    }
+
+    public BankMode currentBankMode;
+
 
     // Use this for initialization
     void Start()
@@ -103,9 +112,20 @@ public class Boid : MonoBehaviour
 
         if (velocity.magnitude > float.Epsilon)
         {
-            Vector3 tempUp = Vector3.Lerp(transform.up, Vector3.up + (acceleration * banking), Time.deltaTime * 3.0f);
-            transform.LookAt(transform.position + velocity, tempUp);
+            if (bank) {
 
+                if (currentBankMode == BankMode.correct)
+                {
+                    Vector3 tempUp = Vector3.Lerp(transform.up, Vector3.up + (acceleration * banking), Time.deltaTime * 3.0f);
+                    transform.LookAt(transform.position + velocity, tempUp);
+                }
+                else if (currentBankMode == BankMode.stay) {
+                    Vector3 tempUp = Vector3.Lerp(transform.up, transform.up + (acceleration * banking), Time.deltaTime * 3.0f);
+                    transform.LookAt(transform.position + velocity, tempUp);
+                }
+                
+            }
+            
             transform.position += velocity * Time.deltaTime;
             velocity *= (1.0f - (damping * Time.deltaTime));
         }
