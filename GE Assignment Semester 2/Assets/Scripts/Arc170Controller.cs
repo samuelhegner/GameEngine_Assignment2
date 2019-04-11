6c47890b898979bf8c0c4bb1a07da75a7230fe99
samuelhegner <C16439262@mydit.ie>
2019-04-11 13:35:53 +0100

    State machine for Arc170 start

---
+++ GE Assignment Semester 2/Assets/Scripts/Arc170Controller.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class LeadAllies : State
+{
+    public override void Enter()
+    {
+    }
+
+    public override void Think()
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+}
+
+class FormationApproach : State
+{
+    public override void Enter()
+    {
+    }
+
+    public override void Think()
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+}
+
+class ChaseDown : State
+{
+    public override void Enter()
+    {
+    }
+
+    public override void Think()
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+}
+
+class HelpAlly : State
+{
+    public override void Enter()
+    {
+    }
+
+    public override void Think()
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+}
+
+class ShakeEnemy : State
+{
+    public override void Enter()
+    {
+    }
+
+    public override void Think()
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+}
+
+public class Arc170Controller : MonoBehaviour
+{
+    StateMachine stateMachine;
+
+    public bool leader;
+
+    public int number;
+
+    void Awake()
+    {
+        stateMachine = GetComponent<StateMachine>();
+        if (leader) {
+            //gameObject.AddComponent
+        }
+    }
+
+    void Start()
+    {
+        if (leader)
+        {
+            stateMachine.ChangeState(new LeadAllies());
+        }
+        else {
+            stateMachine.ChangeState(new FormationApproach());
+        }
+    }
+
+    void Update()
+    {   
+    }
+}
---
+++ GE Assignment Semester 2/Assets/Scripts/StateMachine.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State
+{
+    public StateMachine owner;
+    public virtual void Enter() { }
+    public virtual void Exit() { }
+    public virtual void Think() { }
+}
+
+public class StateMachine : MonoBehaviour
+{
+    bool changingState;
+
+
+    public State currentState;
+    public State globalState;
+    public State previousState;
+
+    private IEnumerator coroutine;
+
+    public int updatesPerSecond = 5;
+
+    private void OnEnable()
+    {
+        StartCoroutine(Think());
+    }
+
+
+    public void ChangeStateDelayed(State newState, float delay)
+    {
+        coroutine = ChangeStateCoRoutine(newState, delay);
+        StartCoroutine(coroutine);
+    }
+
+    public void CancelDelayedStateChange()
+    {
+        if (coroutine != null && changingState)
+        {
+            StopCoroutine(coroutine);
+        }
+    }
+
+    IEnumerator ChangeStateCoRoutine(State newState, float delay)
+    {
+        changingState = true;
+        yield return new WaitForSeconds(delay);
+        ChangeState(newState);
+        changingState = false;
+    }
+
+    public void RevertToPreviousState()
+    {
+        ChangeState(previousState);
+    }
+
+    public void ChangeState(State newState)
+    {
+        previousState = currentState;
+        if (currentState != null)
+        {
+            currentState.Exit();
+        }
+        currentState = newState;
+        currentState.owner = this;
+        Debug.Log(currentState.GetType());
+        currentState.Enter();
+    }
+
+    System.Collections.IEnumerator Think()
+    {
+        yield return new WaitForSeconds(Random.Range(0, 0.5f));
+        while (true)
+        {
+            if (globalState != null)
+            {
+                globalState.Think();
+            }
+            if (currentState != null)
+            {
+                currentState.Think();
+            }
+
+            yield return new WaitForSeconds(1.0f / (float)updatesPerSecond);
+        }
+    }
+}
