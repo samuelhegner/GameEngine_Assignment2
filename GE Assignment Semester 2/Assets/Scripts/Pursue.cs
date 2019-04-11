360128253c7966fe132a5fe688f491ad45ca529f
samuelhegner <C16439262@mydit.ie>
2019-04-11 19:29:20 +0100

    Spawner start

--- GE Assignment Semester 2/Assets/Scripts/Pursue.cs
+++ GE Assignment Semester 2/Assets/Scripts/Pursue.cs
@@ -12,15 +12,6 @@ public class Pursue : SteeringBehaviour
     {
     }
 
-    public void OnDrawGizmos()
-    {
-        if (Application.isPlaying)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawLine(transform.position, targetPos);
-        }
-    }
-
     public override Vector3 Calculate()
     {
         float dist = Vector3.Distance(target.transform.position, transform.position);
---
+++ GE Assignment Semester 2/Assets/SpawnShip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnShip : MonoBehaviour
+{
+    public float spawnRatePerSecond;
+    public bool usePool;
+
+    public GameObject ship;
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    public IEnumerator Spawn() {
+        while (true){
+            if (usePool)
+            {
+                Object_Pool.Instance.SpawnFromPool("Vulture", transform.position, Quaternion.identity);
+            }
+            else {
+                Instantiate(ship, transform.position, Quaternion.identity);
+            }
+            yield return new WaitForSeconds(1f / spawnRatePerSecond);
+        }
+    }
+
+}
