e6b6b86bec2a74914d91729585c3c38be4d6928d
samuelhegner <C16439262@mydit.ie>
2019-04-25 11:00:39 +0100

    Missile Start

---
+++ GE Assignment Semester 2/Assets/MissileController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class MissileIdle : State {
+
+    MissileController controller;
+
+
+    public override void Enter()
+    {
+        controller = owner.GetComponent<MissileController>();
+    }
+
+    public override void Think()
+    {
+        if (controller.launch) {
+
+        }
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
+
+class MissileChase : State
+{
+    public override void Enter()
+    {
+
+    }
+
+    public override void Think()
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
+
+class MissileExplode : State
+{
+    public override void Enter()
+    {
+
+    }
+
+    public override void Think()
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
+
+public class MissileController : MonoBehaviour
+{
+    StateMachine stateMachine;
+
+    public bool launch;
+
+    void Awake()
+    {
+        stateMachine = GetComponent<StateMachine>();
+        stateMachine.ChangeState(new MissileIdle());
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ GE Assignment Semester 2/Assets/SeekFront.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeekFront : SteeringBehaviour
+{
+    public GameObject targetGameObject = null;
+    public Vector3 target = Vector3.zero;
+
+    public float distanceInFront;
+
+    public void OnDrawGizmos()
+    {
+        if (isActiveAndEnabled && Application.isPlaying)
+        {
+            Gizmos.color = Color.cyan;
+            if (targetGameObject != null)
+            {
+                target = targetGameObject.transform.position;
+            }
+            Gizmos.DrawLine(transform.position, target);
+        }
+    }
+
+    public override Vector3 Calculate()
+    {
+        return boid.SeekForce(target);
+    }
+
+    public void Update()
+    {
+        if (targetGameObject != null)
+        {
+            target = targetGameObject.transform.position + (targetGameObject.transform.forward * distanceInFront);
+        }
+    } 
+}
