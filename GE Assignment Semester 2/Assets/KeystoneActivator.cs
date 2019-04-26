5425cfb6cd2f6cc97932b9c72236f525dd2f77ca
samuelhegner <C16439262@mydit.ie>
2019-04-26 11:56:13 +0100

    Scene Changes and adjustments

--- GE Assignment Semester 2/Assets/KeystoneActivator.cs
+++ GE Assignment Semester 2/Assets/KeystoneActivator.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class KeystoneActivator : MonoBehaviour
 {
+    CameraManager cameraManager;
+
+    Path path;
+
+    public int currentWaypoint;
+
     void Start()
     {
-        
+        cameraManager = GameObject.FindObjectOfType<CameraManager>();
+        path = GameObject.FindObjectOfType<Path>();
     }
 
     void Update()
     {
-        
+        currentWaypoint = path.next;
     }
 }
